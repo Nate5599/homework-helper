6f5ed22fc33e61dcca195b778c2bd7a423f47504
Nate5599 <natouli215@gmail.com>
2025-09-28 23:19:37 -0400

    First working commit with all app files

--- main.py
+++ main.py
@@ -413,6 +413,5 @@ if __name__ == "__main__":
     import os
     host = os.environ.get("FLASK_HOST", "0.0.0.0")
     port = int(os.environ.get("PORT", 5000))
-    debug = os.environ.get("FLASK_DEBUG", "1") == "1"  # local default True
-
+    debug = os.environ.get("FLASK_DEBUG", "1") == "1"
     app.run(host=host, port=port, debug=debug)
